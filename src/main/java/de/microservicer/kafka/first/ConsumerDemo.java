e3c5a8727bb447ac3ea949c0fc4981e55fcefa0e
HarjeetSingh <singh.fulda@gmail.com>
2020-09-17 17:04:13 +0200

    add consumer

---
+++ src/main/java/de/microservicer/kafka/first/ConsumerDemo.java
@@ -0,0 +1,52 @@
+package de.microservicer.kafka.first;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConsumerDemo {
+
+	public static void main(String[] args) {
+
+		Logger logger = LoggerFactory.getLogger(ConsumerDemo.class);
+
+		String bootstrapServerString = "127.0.0.1:9092";
+		String groupIdString = "my-fourth-application";
+		String topic = "second_topic";
+
+		// create producer properties
+		Properties properties = new Properties();
+
+		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServerString);
+		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupIdString);
+		properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+		// create consumer
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+		// subscribe consumer
+		consumer.subscribe(Collections.singleton(topic));
+
+		// poll new data
+		while (true) {
+			ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+			for (ConsumerRecord<String, String> record : records) {
+				logger.info("Key: " + record.key() + ", value: " + record.value());
+				logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+
+			}
+		}
+
+	}
+
+}
